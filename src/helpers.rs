df698611bd3c97ab0fc108c56b074d59ddb472d5
Andrea <andrea.cristina.dev@gmail.com>
2026-03-19 19:50:24 +0000

    Cleanup.

--- src/helpers.rs
+++ src/helpers.rs
@@ -1,25 +1,28 @@
-fn left_get(cell: Noun) -> Noun {
+use std::rc::Rc;
+use super::Noun;
+
+pub fn left_get(cell: Noun) -> Noun {
     match cell {
-        Noun::Cell(x, _y) => *x,
+        Noun::Cell(x, _y) => (*x).clone(),
         _ => panic!("Unkown"),
     }
 }
 
-fn right_get(cell: Noun) -> Noun {
+pub fn right_get(cell: Noun) -> Noun {
     match cell {
-        Noun::Cell(_x, y) => *y,
+        Noun::Cell(_x, y) => (*y).clone(),
         _ => panic!("Unkown"),
     }
 }
 
-fn number_get(cell: Noun) -> u64 {
+pub fn number_get(cell: Noun) -> u64 {
     match cell {
         Noun::Atom(x) => x,
         _ => panic!("Unkown"),
     }
 }
 
-fn tree_get(noun: Noun, address: u64) -> Noun {
+pub fn tree_get(noun: Noun, address: u64) -> Noun {
     match address {
         1 => noun,
         _val if address%2 == 0 => {
@@ -31,16 +34,16 @@ fn tree_get(noun: Noun, address: u64) -> Noun {
     }
 }
 
-fn tree_set(noun: Noun, axis: u64, replacement: Noun) -> Noun {
+pub fn tree_set(noun: Noun, axis: u64, replacement: Noun) -> Noun {
     match axis {
         1 => replacement,
         _val if axis%2 == 0 => {
             let sibling = tree_get(noun.clone(), axis + 1);
-            tree_set(noun.clone(), axis/2, Noun::Cell(Box::new(replacement), Box::new(sibling)))
+            tree_set(noun.clone(), axis/2, Noun::Cell(Rc::new(replacement), Rc::new(sibling)))
         }
         _ => {
             let sibling = tree_get(noun.clone(), axis - 1);
-            tree_set(noun.clone(), axis/2, Noun::Cell(Box::new(sibling), Box::new(replacement)))
+            tree_set(noun.clone(), axis/2, Noun::Cell(Rc::new(sibling), Rc::new(replacement)))
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+mod helpers;
+use helpers::left_get;
+use helpers::right_get;
+use helpers::number_get;
+use helpers::tree_get;
+use helpers::tree_set;
 use std::fs;
 use std::rc::Rc;
 
@@ -6,7 +12,7 @@ use std::rc::Rc;
 #[derive(PartialEq)]
 
 
-enum Noun {
+pub enum Noun {
     Atom(u64),
     Cell(Rc<Noun>, Rc<Noun>),
 }
@@ -104,52 +110,6 @@ fn eval(mut subject: Noun, mut formula: Noun) -> Noun {
     }
 }
 
-fn left_get(cell: Noun) -> Noun {
-    match cell {
-        Noun::Cell(x, _y) => (*x).clone(),
-        _ => panic!("Unkown"),
-    }
-}
-
-fn right_get(cell: Noun) -> Noun {
-    match cell {
-        Noun::Cell(_x, y) => (*y).clone(),
-        _ => panic!("Unkown"),
-    }
-}
-
-fn number_get(cell: Noun) -> u64 {
-    match cell {
-        Noun::Atom(x) => x,
-        _ => panic!("Unkown"),
-    }
-}
-
-fn tree_get(noun: Noun, address: u64) -> Noun {
-    match address {
-        1 => noun,
-        _val if address%2 == 0 => {
-            left_get(tree_get(noun, address / 2))
-          }
-        _ => {
-            right_get(tree_get(noun, address / 2))
-          }
-    }
-}
-
-fn tree_set(noun: Noun, axis: u64, replacement: Noun) -> Noun {
-    match axis {
-        1 => replacement,
-        _val if axis%2 == 0 => {
-            let sibling = tree_get(noun.clone(), axis + 1);
-            tree_set(noun.clone(), axis/2, Noun::Cell(Rc::new(replacement), Rc::new(sibling)))
-        }
-        _ => {
-            let sibling = tree_get(noun.clone(), axis - 1);
-            tree_set(noun.clone(), axis/2, Noun::Cell(Rc::new(sibling), Rc::new(replacement)))
-        }
-    }
-}
 
 fn serialize(noun: Noun) -> Vec<u8> {
     match noun {
